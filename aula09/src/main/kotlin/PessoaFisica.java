00cb24ea23c0c46c161234990ff018e5eff7d0a4
myusername <myemail>
2022-02-15 22:00:58 -0300

    aula 9

---
+++ aula09/src/main/kotlin/PessoaFisica.java
@@ -0,0 +1,13 @@
+class PessoaFisica(
+    // Construtor da pessoa Física
+    var cpf:String,
+    var nome:String,
+    var rg:String
+    // Info da pessoa
+    matricula:Short,
+    endereco:String,
+    telefone:String,
+    email: String
+) : Pessoa(
+    // Construtor da pessoa
+)
